cae7350ed12a7908edbe7b1ad2146a6e239ea37a
micraft <vbotvin42@gmail.com>
2015-01-12 21:49:36 +0300

    Update main.php

--- main.php
+++ main.php
@@ -2,16 +2,8 @@
 <html>
 <head>
 <meta charset="utf-8">
-
 <title>iCloud Brooter | iHateApple</title>
 
-
-
-<script type="text/javascript">
-//<![CDATA[
-try{if (!window.CloudFlare) {var CloudFlare=[{verbose:0,p:1420825962,byc:0,owlid:"cf",bag2:1,mirage2:0,oracle:0,paths:{cloudflare:"/cdn-cgi/nexp/dok2v=1613a3a185/"},atok:"eb79b374014e1b09dc868279d62dbb4f",petok:"051f34046d926912f0f10f05a1c4a0d3898394aa-1421088282-1800",zone:"ihateapple.co.uk",rocket:"0",apps:{}}];CloudFlare.push({"apps":{"ape":"28f6e0bc1b95ca79cc0fc76c3f281ea3"}});!function(a,b){a=document.createElement("script"),b=document.getElementsByTagName("script")[0],a.async=!0,a.src="//ajax.cloudflare.com/cdn-cgi/nexp/dok2v=919620257c/cloudflare.min.js",b.parentNode.insertBefore(a,b)}()}}catch(e){};
-//]]>
-</script>
 <style type="text/css">
 body {
     background-color: #FFFFFF;
