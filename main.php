69b5591e92cd853c4c99c3f66fdd206a13021639
micraft <vbotvin42@gmail.com>
2015-01-12 22:43:27 +0300

    Create main.php

---
+++ main.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+
+<title>iDict</title>
+
+<link rel="icon" type="image/vnd.microsoft.icon"
+     href="images/favicon.ico" />
+
+<style type="text/css">
+body {
+    background-color: #000000;
+}
+table, th, td {
+    border: 1px solid black;
+}
+h1 {
+    color: #FFFFFF;
+} 
+</style>
+</head>
+
+<body>
+<center>
+<a><img src="images/lax.png"></a>
+
+
+
+
+
+<br>
+<b><h1>=================================================
+<br>|  iDict Apple ID BruteForcer and Account Management Tool  |
+<br>=================================================
+<b><br>
+
+ 
+
+
+
+
+
+Trying URL:<br>~=~https://setup.icloud.com/setup/iosbuddy/loginDelegates~=~<br></b>Trying Password:<br>~=~Password1
+~=~<br><br><b>Аккаунт заблокирован!</b><br>
